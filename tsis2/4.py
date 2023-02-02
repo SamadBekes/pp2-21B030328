8f5569c2a9ccbbeadb47d63bbb34abfc521d02b9
Samad Bekesov <samadbekesov@gmail.com>
2023-02-02 23:19:09 +0600

    second job

---
+++ tsis2/4.py
@@ -0,0 +1,7 @@
+cars = ["Ford", "Volvo", "BMW"]
+cars.append("Honda")
+cars.pop(1)
+cars.remove("Volvo")
+x = len(cars)
+for x in cars:
+  print(x)
---
+++ tsis2/6.py
@@ -0,0 +1,17 @@
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+thisset = {"apple", "banana", "cherry", "apple"}
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+print(len(thisset))
+
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
+
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+
